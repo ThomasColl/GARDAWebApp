f7b656d60b60805995e04867963e6be1eb3954b8
Tom <trlc1996@hotmail.com>
2019-01-13 01:08:13 +0000

    Updating background items, adding in basics

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/items')
+def items():
+    return render_template('items.html')
+
+
+@app.route('/preferences')
+def preferences():
+    return render_template('preferences.html')
+
+
+@app.route('/analytics')
+def analytics():
+    return render_template('analytics.html')
